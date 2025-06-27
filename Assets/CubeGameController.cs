8308c7ef55e3d52b1f33f7adcf8a5b265449a65a
aquaborn679 <79850051+aquaborn679@users.noreply.github.com>
2025-06-27 15:06:39 +0500

    added assets

---
+++ Assets/CubeGameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CubeGameController : MonoBehaviour
+{
+    public Rigidbody cube1, cube2, cube3;
+    public GameObject winText;
+    public int correctCubeIndex = 1; // от 1 до 3
+
+    public void SelectCube(int index)
+    {
+        Debug.Log("Pressed: " + index); 
+
+        cube1.useGravity = index != 1;
+        cube2.useGravity = index != 2;
+        cube3.useGravity = index != 3;
+
+        if (index == correctCubeIndex)
+            winText.SetActive(true);
+    }
+
+}
